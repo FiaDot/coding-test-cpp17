4dad8889dc08c6c3abcd15c7a498ee4fdbc7ae0d
LEE GUN HO <jason@grampus.co>
2023-03-22 11:30:02 +0900

    문자와 문자열 : https://www.acmicpc.net/problem/27866

---
+++ bakjoon/string/print_at.cpp
@@ -0,0 +1,20 @@
+//
+// Created by LEE GUNHO on 2023/03/22.
+//
+
+// 문자와 문자열 : https://www.acmicpc.net/problem/27866
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+
+    cout << s[n-1];
+    return 0;
+}
+
