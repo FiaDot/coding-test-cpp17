dbc5fc1530968870d437d8770e4204f8299625ba
LEE GUN HO <jason@grampus.co>
2023-03-22 11:33:30 +0900

    단어 길이 재기 : https://www.acmicpc.net/problem/2743

---
+++ bakjoon/string/length.cpp
@@ -0,0 +1,16 @@
+//
+// Created by LEE GUNHO on 2023/03/22.
+//
+
+// 단어 길이 재기 : https://www.acmicpc.net/problem/2743
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s.length();
+    return 0;
+}
