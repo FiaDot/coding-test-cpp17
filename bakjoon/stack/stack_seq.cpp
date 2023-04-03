3cad92b708500a06cbb674cc7db63bdce2ee1fc5
LEE GUN HO <jason@grampus.co>
2023-04-03 16:18:29 +0900

    스택 수열 : https://www.acmicpc.net/problem/1874 (다른 코드중에 반례)

--- bakjoon/stack/stack_seq.cpp
+++ bakjoon/stack/stack_seq.cpp
@@ -82,3 +82,52 @@ int main()
     }
     return 0;
 }
+
+/*
+ 심플하게 하는군 ;; ..... but 1632 에서 실패
+  https://gaeunhan.tistory.com/16
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	stack<int> s;
+	vector<char> result;
+	int cnt = 1;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+
+		while ( cnt <= x )
+		{
+			s.push(cnt);
+			cnt += 1;
+			result.push_back( '+' );
+		}
+
+		if (s.top() == x)
+		{
+			s.pop();
+			result.push_back( '-' );
+		}
+		else
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << '\n'; //!< endl로 인한 시간초과 조심
+	}
+}
+
+ */
